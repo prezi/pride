5719e6326883fd9ca77c609a2cd9a4f5f05f50cd
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-01 02:23:19 +0200

    Add .pride/modules during init

--- src/main/groovy/com/prezi/gradle/pride/SessionInitializer.groovy
+++ src/main/groovy/com/prezi/gradle/pride/SessionInitializer.groovy
@@ -7,6 +7,9 @@ import org.gradle.tooling.model.gradle.GradleBuild
  * Created by lptr on 31/03/14.
  */
 class SessionInitializer {
+	public static final String PRIDE_DIRECTORY = ".pride"
+	public static final String PRIDE_MODULES = "modules"
+
 	public static final String SETTINGS_GRADLE = "settings.gradle"
 	public static final String BUILD_GRADLE = "build.gradle"
 	public static final String GRADLE_PROPERTIES = "gradle.properties"
@@ -23,40 +26,44 @@ class SessionInitializer {
 		def settingsFile = new File(sessionDirectory, SETTINGS_GRADLE)
 		def buildFile = new File(sessionDirectory, BUILD_GRADLE)
 		def gradleProperties = new File(sessionDirectory, GRADLE_PROPERTIES)
+		def prideDirectory = new File(sessionDirectory, PRIDE_DIRECTORY)
+		def prideModulesFile = new File(prideDirectory, PRIDE_MODULES)
 
-		def sessionExists = settingsFile.exists() || buildFile.exists() || gradleProperties.exists()
+		def sessionExists = prideDirectory.exists() || settingsFile.exists() || buildFile.exists() || gradleProperties.exists()
 		if (!overwrite && sessionExists) {
 			throw new PrideException("A session already exists in ${sessionDirectory}")
 		}
 
 		System.out.println((sessionExists ? "Reinitializing" : "Initializing") + " ${sessionDirectory}")
 		sessionDirectory.mkdirs()
+		prideDirectory.deleteDir()
+		prideDirectory.mkdirs()
 		settingsFile.delete()
 		buildFile.delete()
 		gradleProperties.delete()
 
-		sessionDirectory.eachDir { dir ->
-			if (isValidProject(dir)) {
-				def connection = gradleConnector.get().forProjectDirectory(dir).connect()
+		sessionDirectory.eachDir { moduleDirectory ->
+			if (isValidProject(moduleDirectory)) {
+				def connection = gradleConnector.get().forProjectDirectory(moduleDirectory).connect()
 				try {
-					def relativePath = sessionDirectory.toURI().relativize(dir.toURI()).toString()
+					def relativePath = sessionDirectory.toURI().relativize(moduleDirectory.toURI()).toString()
 
 					// Load the model for the build
 					GradleBuild build = connection.getModel(GradleBuild)
 
 					// Merge settings
 					settingsFile << "\n// Settings from project in directory /${relativePath}\n\n"
-					build.projects.each { prj ->
-						if (prj == build.rootProject) {
-							settingsFile << "include '$build.rootProject.name'\n"
-							settingsFile << "project(':$build.rootProject.name').projectDir = file('$dir.name')\n"
+					build.projects.each { project ->
+						if (project == build.rootProject) {
+							settingsFile << "include '${build.rootProject.name}'\n"
+							settingsFile << "project(':${build.rootProject.name}').projectDir = file('${moduleDirectory.name}')\n"
 						} else {
-							settingsFile << "include '$build.rootProject.name$prj.path'\n"
+							settingsFile << "include '${build.rootProject.name}${project.path}'\n"
 						}
 					}
 
 					// Merge gradle.properties
-					def localGradleProperties = new File(dir, GRADLE_PROPERTIES)
+					def localGradleProperties = new File(moduleDirectory, GRADLE_PROPERTIES)
 					if (localGradleProperties.exists()) {
 						def localGradlePropertiesText = localGradleProperties.text
 						if (!localGradlePropertiesText.endsWith("\n")) {
@@ -65,6 +72,9 @@ class SessionInitializer {
 						gradleProperties << "\n# Properties from project in directory /${relativePath}\n\n"
 						gradleProperties << localGradlePropertiesText
 					}
+
+					// Add module to .pride/modules
+					prideModulesFile << "${moduleDirectory.name}\n"
 				} finally {
 					// Clean up
 					connection.close()
