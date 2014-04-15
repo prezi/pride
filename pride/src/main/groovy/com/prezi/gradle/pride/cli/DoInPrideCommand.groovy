91557007a269501ac194d8808c9fa8865ab91a22
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-15 16:41:20 +0200

    Fixed #11
    `pride do` now takes --include (-I) and --exclude parameters.

--- pride/src/main/groovy/com/prezi/gradle/pride/cli/DoInPrideCommand.groovy
+++ pride/src/main/groovy/com/prezi/gradle/pride/cli/DoInPrideCommand.groovy
@@ -4,19 +4,36 @@ import com.prezi.gradle.pride.Pride
 import com.prezi.gradle.pride.PrideException
 import io.airlift.command.Arguments
 import io.airlift.command.Command
+import io.airlift.command.Option
 
 /**
  * Created by lptr on 10/04/14.
  */
-@Command(name = "do", description = "Execute a command in all modules in a pride")
+@Command(name = "do", description = "Execute a command in all modules, or a subset of the modules in a pride")
 class DoInPrideCommand extends PrideCommand {
+	@Option(name = ["-I", "--include"],
+			title = "repo",
+			description = "Execute the command on repo (can be specified multiple times)")
+	private List<File> inlcudeRepos
+
+	@Option(name = "--exclude",
+			title = "repo",
+			description = "Do not execute command on repo (can be specified multiple times)")
+	private List<File> excludeRepos
+
 	@Arguments(required = true, description = "The command to execute")
 	private List<String> commandLine
 
 	@Override
 	void run() {
 		Pride pride = new Pride(prideDirectory)
-		pride.modules.each { moduleDirectory ->
+		def modules = (inlcudeRepos ? inlcudeRepos : pride.modules).sort { it.name }.findAll { includeRepo ->
+			return null == excludeRepos.find { excludeRepo ->
+				includeRepo.absoluteFile.equals(excludeRepo.absoluteFile)
+			}
+		}
+
+		modules.each { moduleDirectory ->
 			System.out.println("\n${moduleDirectory} \$ ${commandLine.join(" ")}")
 			def process = commandLine.execute((String[]) null, moduleDirectory)
 			process.waitForProcessOutput((OutputStream) System.out, System.err)
