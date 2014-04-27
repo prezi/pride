1971ebf188ccd94efc72779a4bc3988e7d0cad07
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-27 13:29:06 +0200

    Removed scripts directory
    We are only building in Travis now, no need for the Jenkins stuff

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-
-if [ -e /etc/profile.d/gradle.sh ]; then
-    . /etc/profile.d/gradle.sh
-fi
-
-gradle clean uploadArchives --info --stacktrace -Prelease
