9f351ee677dfb30f9e4c4ca53f69ec5b0642dc7f
Lorant Pinter <lorant.pinter@prezi.com>
2014-04-28 22:26:52 +0200

    Added test for multi-project dependency resolution

--- gradle-pride-plugin/src/test/groovy/com/prezi/gradle/pride/PridePluginTest.groovy
+++ gradle-pride-plugin/src/test/groovy/com/prezi/gradle/pride/PridePluginTest.groovy
@@ -26,6 +26,37 @@ class PridePluginTest extends Specification {
 		dependencies(project, "compile") == ["com.example:example-test:1.0"]
 	}
 
+	def "multi project dependencies"() {
+		def root = ProjectBuilder.builder().withName("root").build()
+		def childA = ProjectBuilder.builder().withParent(root).withName("childA").build()
+		ProjectBuilder.builder().withParent(root).withName("childB").build()
+
+		root.allprojects {
+			group = "com.example"
+		}
+
+		childA.apply plugin: "pride"
+		childA.configurations {
+			compile
+			test
+		}
+		childA.configure(childA.extensions.getByType(DynamicDependenciesExtension)) {
+			compile "com.example:example-test:1.0", {
+				force = true
+			}
+			compile "com.example:childB:2.0", {
+				force = true
+			}
+			test (group: "com.example", name: "childB", version: "2.0", configuration: "test")
+		}
+		evaluateAllProjects(root.gradle)
+
+		expect:
+		dependencies(childA, "compile") == ["com.example:example-test:1.0", ":childB@default"]
+		dependencies(childA, "test") == [":childB@test"]
+		childA.configurations.getByName("compile").dependencies.find { it.name == "example-test" }.force == true
+	}
+
 	private static List<String> dependencies(Project project, String configuration) {
 		project.configurations.getByName(configuration).dependencies.collect { Dependency dep ->
 			if (dep instanceof ProjectDependency) {
