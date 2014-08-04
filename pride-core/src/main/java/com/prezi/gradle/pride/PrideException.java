1e5b1e91d9fc82f3335fa9bb7857ba28fc621b1c
Lorant Pinter <lorant.pinter@prezi.com>
2014-08-05 00:30:47 +0200

    Remove Groovy-era comment

--- pride-core/src/main/java/com/prezi/gradle/pride/PrideException.java
+++ pride-core/src/main/java/com/prezi/gradle/pride/PrideException.java
@@ -1,12 +1,5 @@
 package com.prezi.gradle.pride;
 
-/**
- * This exception has been converted to a Java class, because
- * if it's a Groovy class, Pride will throw an ugly exception:
- * https://github.com/prezi/pride/issues/47
- *
- * Created by lptr on 31/03/14.
- */
 public class PrideException extends RuntimeException {
 	public PrideException()
 	{
