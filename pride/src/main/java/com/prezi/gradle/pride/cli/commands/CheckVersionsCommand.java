49e95ed7f05a4ed36817d2bc1addbb83f04502ef
Lorant Pinter <lorant.pinter@prezi.com>
2014-10-06 12:44:10 +0200

    Run check-versions only on top-level project

--- pride/src/main/java/com/prezi/gradle/pride/cli/commands/CheckVersionsCommand.java
+++ pride/src/main/java/com/prezi/gradle/pride/cli/commands/CheckVersionsCommand.java
@@ -2,64 +2,52 @@ package com.prezi.gradle.pride.cli.commands;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.prezi.gradle.pride.Module;
 import com.prezi.gradle.pride.Pride;
 import com.prezi.gradle.pride.PrideException;
 import com.prezi.gradle.pride.RuntimeConfiguration;
 import com.prezi.gradle.pride.cli.gradle.GradleConnectorManager;
 import com.prezi.gradle.pride.cli.model.ProjectModelAccessor;
-import com.prezi.gradle.pride.internal.LoggedNamedProgressAction;
-import com.prezi.gradle.pride.internal.ProgressUtils;
 import com.prezi.gradle.pride.projectmodel.DynamicDependency;
 import com.prezi.gradle.pride.projectmodel.PrideProjectModel;
-import io.airlift.command.Arguments;
 import io.airlift.command.Command;
 
-import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 @Command(name = "check-versions", description = "Check if dependency and module versions match")
-public class CheckVersionsCommand extends AbstractFilteredPrideCommand {
-
-	@Arguments(description = "Modules to check")
-	private List<String> includeModules;
+public class CheckVersionsCommand extends AbstractPrideCommand {
 
 	@Override
-	protected void executeInModules(Pride pride, Collection<Module> modules) throws Exception {
+	public void executeInPride(Pride pride) throws Exception {
 		RuntimeConfiguration config = pride.getConfiguration();
 		final ProjectModelAccessor modelAccessor = ProjectModelAccessor.create(new GradleConnectorManager(config), isVerbose());
 		final Set<PrideProjectModel> models = Sets.newLinkedHashSet();
-		ProgressUtils.execute(pride, modules, new LoggedNamedProgressAction<Module>("Checking dependency versions in module") {
-			@Override
-			protected void execute(Pride pride, Module module) throws IOException {
-				PrideProjectModel rootModel = modelAccessor.getRootProjectModel(pride.getModuleDirectory(module.getName()));
-				addWithChildren(rootModel, models);
-			}
-		});
+		PrideProjectModel prideModel = modelAccessor.getRootProjectModel(pride.getRootDirectory());
+		addChildModels(prideModel, models);
 
 		Map<String, PrideProjectModel> projectsByGroupAndName = Maps.newTreeMap();
 		for (PrideProjectModel model : models) {
 			projectsByGroupAndName.put(getId(model), model);
 		}
-		boolean problem = false;
+		Set<String> versionConflicts = Sets.newLinkedHashSet();
 		for (PrideProjectModel model : models) {
 			for (Set<DynamicDependency> dependencies : model.getDynamicDependencies().values()) {
 				for (DynamicDependency dependency : dependencies) {
 					PrideProjectModel dependencyModel = projectsByGroupAndName.get(getId(dependency));
 					if (dependencyModel != null) {
 						if (!matchVersion(dependency.getVersion(), dependencyModel.getVersion())) {
-							logger.warn("Project \"{}\" requests version {} of project \"{}\", but its current version ({}) does not fulfill that request", getId(model), dependency.getVersion(), getId(dependencyModel), dependencyModel.getVersion());
-							problem = true;
+							String conflict = String.format("Project \"%s\" requests version %s of project \"%s\", but its current version (%s) does not fulfill that request", getId(model), dependency.getVersion(), getId(dependencyModel), dependencyModel.getVersion());
+							versionConflicts.add(conflict);
 						}
 					}
 				}
 			}
 		}
-		if (problem) {
+		if (!versionConflicts.isEmpty()) {
+			for (String conflict : versionConflicts) {
+				logger.warn(conflict);
+			}
 			throw new PrideException("There are modules that refer to dependency versions that wouldn't work outside this pride. See warnings above.");
 		}
 	}
@@ -71,15 +59,10 @@ public class CheckVersionsCommand extends AbstractFilteredPrideCommand {
 		return requestedPattern.matcher(actual).matches();
 	}
 
-	@Override
-	protected Collection<String> getIncludeModules() {
-		return null;
-	}
-
-	private static void addWithChildren(PrideProjectModel model, Set<PrideProjectModel> models) {
-		models.add(model);
+	private static void addChildModels(PrideProjectModel model, Set<PrideProjectModel> models) {
 		for (PrideProjectModel childModel : model.getChildren()) {
-			addWithChildren(childModel, models);
+			models.add(childModel);
+			addChildModels(childModel, models);
 		}
 	}
 
