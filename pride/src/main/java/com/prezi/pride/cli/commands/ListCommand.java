b7a7b5cb9adf5d6a3659614a71db6172c3637491
Simone Fonda <simone.fonda@prezi.com>
2015-02-21 17:51:55 +0100

    list command displays module and branch names in columns

--- pride/src/main/java/com/prezi/pride/cli/commands/ListCommand.java
+++ pride/src/main/java/com/prezi/pride/cli/commands/ListCommand.java
@@ -56,9 +56,26 @@ public class ListCommand extends AbstractFilteredPrideCommand {
 	private static class StatusFormatter implements LineFormatter {
 
 		private final Pride pride;
+		private int moduleMaxLength = 0;
+		private int branchMaxLength = 0;
 
 		private StatusFormatter(Pride pride) {
 			this.pride = pride;
+
+			Collection<Module> modules = pride.getModules();
+			for (Module module : modules) {
+				String name = module.getName();
+				moduleMaxLength = Math.max(moduleMaxLength, name.length());
+
+				try {
+					String branch = module.getVcs().getSupport().getStatus(pride.getModuleDirectory(name)).getBranch();
+					if (!Strings.isNullOrEmpty(branch)) {
+						branchMaxLength = Math.max(branchMaxLength, branch.length());
+					}
+				} catch (Exception e) {
+					// No branch
+				}
+			}
 		}
 
 		@Override
@@ -68,12 +85,17 @@ public class ListCommand extends AbstractFilteredPrideCommand {
 			VcsStatus status = module.getVcs().getSupport().getStatus(moduleDirectory);
 			String branch = status.getBranch();
 			StringBuilder line = new StringBuilder();
-			line.append(status.hasUnpublishedChanges() ? 'M' : ' ');
-			line.append(status.hasUncommittedChanges() ? 'M' : ' ');
+
+			line.append(status.hasUnpublishedChanges() ? 'P' : ' ');
+			line.append(status.hasUncommittedChanges() ? 'c' : ' ');
 			line.append(' ').append(module.getName());
-			line.append(' ');
+
+			line.append(new String(new char[moduleMaxLength + 2 - module.getName().length()]).replace("\0", " "));
+
 			if (!Strings.isNullOrEmpty(branch)) {
-				line.append(branch).append('@');
+				line.append(branch);
+				line.append(new String(new char[branchMaxLength + 2 - branch.length()]).replace("\0", " "));
+				line.append('@');
 			}
 			line.append(status.getRevision());
 			line.append(" (").append(module.getVcs().getType()).append(")");
