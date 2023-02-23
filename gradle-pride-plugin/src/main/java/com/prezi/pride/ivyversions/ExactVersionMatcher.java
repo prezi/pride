3ba47da142fcb922febfa5f8741544d626652aa6
Sandor Fehervari <sandor.fehervari@prezi.com>
2023-02-23 18:49:47 +0100

    Fix java compiler warnings

--- gradle-pride-plugin/src/main/java/com/prezi/pride/ivyversions/ExactVersionMatcher.java
+++ gradle-pride-plugin/src/main/java/com/prezi/pride/ivyversions/ExactVersionMatcher.java
@@ -10,7 +10,7 @@ import java.util.Map;
  */
 public class ExactVersionMatcher implements VersionMatcher {
     private static final Map<String, Integer> SPECIAL_MEANINGS =
-            ImmutableMap.of("dev", new Integer(-1), "rc", new Integer(1), "final", new Integer(2));
+            ImmutableMap.of("dev", -1, "rc", 1, "final", 2);
 
 	@Override
     public boolean canHandle(String selector) {
